4849dc0a1811d3329f016ccf61db0ef8fa8c64b7
unknown <matheus.oliveira@RPIWSELOGI03.tre-pi.gov.br>
2017-02-20 12:24:49 -0300

    pagina de linhas feita

--- bus/urls.py
+++ bus/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     url(r'^linhas/(?P<pk>[0-9]+)/$', views.veiculo_especifico),
     url(r'^linhasestaticas/$',views.linhas_estaticas),
     url(r'^todasaslinhasestaticas/$',views.todas_linhas_estaticas),
+    url(r'^linha/zona/(?P<pk>[^/]+)/$', views.linhas_por_zona),
 ]
--- bus/views.py
+++ bus/views.py
@@ -34,6 +34,14 @@ cb['X-Auth-Token'] = json.loads(token.text)['token']
 
 # Create your views here.
 
+def verifica_token():
+	global token
+	now = datetime.now().minute
+	if (now - init >= 9):
+		token = pegar_token()
+	else:
+		print("Nao precisa de nova requisicao")
+
 
 @api_view(['GET', 'POST'])
 def reclamacoes(request):
@@ -55,8 +63,6 @@ def linhas(request):
 def post_list(request):
 	global token
 	now = datetime.now().minute
-	#print(init)
-	#print(now)
 	if (now - init >= 9):
 		token = pegar_token()
 	else:
@@ -64,7 +70,10 @@ def post_list(request):
 
 	return HttpResponse(token.text)
 
+
+
 def linhas_estaticas(request):
+	verifica_token()
 	url = "https://api.inthegra.strans.teresina.pi.gov.br/v1/linhas"
 	data = requests.get(url,proxies=proxies, data=json.dumps(dados),headers = cb)
 	lin = json.loads(data.text)
@@ -167,6 +176,7 @@ def linhas_estaticas(request):
 
 
 def todos_veiculos(request,pk):
+	verifica_token()
 	url = "https://api.inthegra.strans.teresina.pi.gov.br/v1/"
 	busca = request.GET.get('busca')
 	if busca is None:
@@ -198,6 +208,7 @@ def todos_veiculos(request,pk):
 
 @api_view(['GET', 'POST'])
 def todas_linhas_estaticas(request):
+	verifica_token()
 	if request.method == 'GET':
 		linha = LinhaOnibus.objects.all()
 		serializer = LinhaOnibusSerializers(linha, many=True)
@@ -206,7 +217,16 @@ def todas_linhas_estaticas(request):
 @api_view(['GET', 'POST'])
 def veiculo_especifico(request,pk):
 	print("aqui")
+	verifica_token()
 	if request.method == 'GET':
 		linha = Linha.objects.filter(CodigoLinha=pk)
 		serializer = LinhaSerializers(linha, many=True)
+		return Response(serializer.data)
+
+@api_view(['GET', 'POST'])
+def linhas_por_zona(request,pk):
+	verifica_token()
+	if request.method == 'GET':
+		linha = LinhaOnibus.objects.filter(Zona=pk)
+		serializer = LinhaOnibusSerializers(linha, many=True)
 		return Response(serializer.data)
